fd83aa3787a6b5fec263dcb1ac21b66291aae3e3
Andrew Sun <andrew@crossbowffs.com>
2024-02-01 05:43:23 -0800

    Update static ip name

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(dirname "$0")"
 
 VAR_DOMAIN_NAME=crossbowffs.net
 VAR_HOSTNAME=minori
-VAR_STATIC_IP_NAME=minori-ip
+VAR_STATIC_IP_NAME=minori-ip-2
 VAR_SSH_KEY_NAME=andrew-sachiko
 VAR_LIGHTSAIL=true
 VAR_UNIXNAME=admin
